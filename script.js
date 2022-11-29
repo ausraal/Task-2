c49154627d42cb6e90742393d6f4ca9630c9e1df
ausraal <ausra.al@gmail.com>
2022-11-29 11:55:24 +0200

    Task 2 completed

---
+++ script.js
@@ -0,0 +1,12 @@
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
+const button = document.querySelector(".btn");
+let count = 0;
+button.addEventListener ("click", () => {
+count += 1;
+let clicked = document.getElementById("btn__state");
+clicked.innerText = count;
+})
